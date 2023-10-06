b5a65365e9f0488305360bd40a57aeb6cba800bc
Carlos Eduardo <127683028+krarlos@users.noreply.github.com>
2023-10-06 20:00:53 +0000

    avances

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  document.querySelector("#excuse").innerHTML = excuseGen();
   console.log("Hello Rigo from the console!");
+};
+
+let excuseGen = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndice = who.length;
+  let actionIndice = action.length;
+  let whatIndice = what.length;
+  let whenIndice = when.length;
+
+  return whoIndice + actionIndice + whatIndice + whenIndice;
 };
